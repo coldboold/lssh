a20bf6be677fea479a008cfb5de762a178ec2df4
coldboold <taochong168@qq.com>
2015-08-23 22:51:05 +0800

    ...

---
+++ src/main/java/com/coldboold/controller/wechat/MyEventHandle.java
@@ -0,0 +1,21 @@
+package com.coldboold.controller.wechat;
+
+import com.github.sd4324530.fastweixin.handle.EventHandle;
+import com.github.sd4324530.fastweixin.message.BaseMsg;
+import com.github.sd4324530.fastweixin.message.req.BaseEvent;
+
+public class MyEventHandle implements EventHandle {
+
+	@Override
+	public BaseMsg handle(BaseEvent event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean beforeHandle(BaseEvent event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/main/java/com/coldboold/controller/wechat/MyMessageHandle.java
@@ -0,0 +1,21 @@
+package com.coldboold.controller.wechat;
+
+import com.github.sd4324530.fastweixin.handle.MessageHandle;
+import com.github.sd4324530.fastweixin.message.BaseMsg;
+import com.github.sd4324530.fastweixin.message.req.BaseReqMsg;
+
+public class MyMessageHandle implements MessageHandle {
+
+	@Override
+	public BaseMsg handle(BaseReqMsg message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean beforeHandle(BaseReqMsg message) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
